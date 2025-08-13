e84163c8e470afadcbfe830787fc68c1004c3a3b
Pushpam <davidpushpam30@gmail.com>
2025-08-14 00:53:16 +0530

    Trying Again to deploy it

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,12 @@
 import path from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Recreate __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+  
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
